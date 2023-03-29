6680c9caea16bae4911144db4bde5a00951a584a
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-29 18:16:37 +0500

    function based views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from .views import get_all
+from .views import (
+    get_all,
+    get_task,
+    mark_task,
+    get_completed_tasks,
+    get_incompleted_tasks,
+    create_task,
+    update_task,
+    delete_task,
 
+)
 
 urlpatterns = [
-    path('tasks', get_all)
+    path('tasks', get_all),
+    path('tasks/<int:id>', get_task),
+    path('tasks/<int:id>/complete', mark_task),
+    path('tasks/completed', get_completed_tasks),
+    path('tasks/incompleted', get_incompleted_tasks),
+    path('tasks/incompleted', get_incompleted_tasks),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpRequest, JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+import json
 from .models import Task
+
+
+def to_dict(task: Task) -> dict:
+    '''to dict'''
+    return {
+        'id': task.id,
+        'name': task.name,
+        'completed': task.completed,
+        'desciption': task.description,
+        'created': task.created,
+        'updated': task.updated,
+    }
 
 def get_all(request: HttpRequest) -> JsonResponse:
     '''get all tasks'''
@@ -7,13 +21,84 @@ def get_all(request: HttpRequest) -> JsonResponse:
     data = {'tasks': []}
 
     for task in tasks:
-        data['tasks'].append({
-            'id': task.id,
-            'name': task.name,
-            'completed': task.completed,
-            'desciption': task.description,
-            'created': task.created,
-            'updated': task.updated,
-        })
+        data['tasks'].append(to_dict(task))
 
     return JsonResponse(data)
+
+def get_task(request: HttpRequest, id: int) -> JsonResponse:
+    '''get task by id'''
+    try:
+        task = Task.objects.get(id=id)
+        return JsonResponse(to_dict(task))
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'Task not found'}, status=404)
+
+def create_task(request: HttpRequest) -> JsonResponse:
+    '''create task'''
+    data = request.body.decode('utf-8')
+    data = json.loads(data)
+    task = Task.objects.create(
+        name=data['name'],
+        description=data['description'],
+        completed=data['completed']
+    )
+    return JsonResponse(to_dict(task))
+
+def mark_task(request: HttpRequest, id: int) -> JsonResponse:
+    '''mark task'''
+    try:
+        task = Task.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'Task not found'}, status=404)
+    
+    task.completed = not task.completed
+    task.save()
+
+    return JsonResponse(to_dict(task))
+
+def get_completed_tasks(request: HttpRequest) -> JsonResponse:
+    '''get tasks by completed'''
+    tasks = Task.objects.filter(completed=True)
+    data = {'tasks': []}
+
+    for task in tasks:
+        data['tasks'].append(to_dict(task))
+
+    return JsonResponse(data)
+
+def get_incompleted_tasks(request: HttpRequest) -> JsonResponse:
+    '''get tasks by completed'''
+    tasks = Task.objects.filter(completed=False)
+    data = {'tasks': []}
+
+    for task in tasks:
+        data['tasks'].append(to_dict(task))
+
+    return JsonResponse(data)
+
+def update_task(request: HttpRequest, id: int) -> JsonResponse:
+    '''update task'''
+    try:
+        task = Task.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'Task not found'}, status=404)
+
+    data = request.body.decode('utf-8')
+    data = json.loads(data)
+
+    task.name = data['name']
+    task.description = data['description']
+    task.completed = data['completed']
+    task.save()
+
+    return JsonResponse(to_dict(task))
+
+def delete_task(request: HttpRequest, id: int) -> JsonResponse:
+    '''delete task'''
+    try:
+        task = Task.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'Task not found'}, status=404)
+
+    task.delete()
+    return JsonResponse({'message': 'Task deleted'})
